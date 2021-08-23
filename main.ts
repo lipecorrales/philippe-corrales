cadf329a398f4c2a222cbc2a3c12923b698f3cb2
lipecorrales <philippecorrales@gmail.com>
2021-08-23 09:25:37 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    variavel = variavel + 1
+    basic.showNumber(variavel)
+})
+input.onButtonPressed(Button.B, function () {
+    variavel = variavel - 1
+    basic.showNumber(variavel)
+})
+let variavel = 0
+variavel = 5
